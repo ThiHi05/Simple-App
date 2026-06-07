668f67f9e4a7a40357e7d3715ff8d20ada55f52d
ThiHi05 <23130098@st.hcmuaf.edu.vn>
2026-06-07 21:51:06 +0700

    Create TestRestart.java

---
+++ src/restart/TestRestart.java
@@ -0,0 +1,46 @@
+package test.restart;
+
+import restart.RestartService;
+import startgame.GameEngine;
+
+public class TestRestart {
+
+//    public static void main(String[] args) {
+//
+//        GameEngine engine =
+//                new GameEngine();
+//
+//        engine.init();
+//
+//        engine.score = 10;
+//
+//        engine.dx = 1;
+//
+//        RestartService restart =
+//                new RestartService();
+//
+//        restart.restart(engine);
+//
+//        System.out.println(
+//                "Score: "
+//                        + engine.score
+//        );
+//
+//        System.out.println(
+//                "Direction: "
+//                        + engine.dx
+//                        + ", "
+//                        + engine.dy
+//        );
+//
+//        System.out.println(
+//                "Snake Length: "
+//                        + engine.snake.size()
+//        );
+//
+//        System.out.println(
+//                "Running: "
+//                        + engine.running
+//        );
+//    }
+}
