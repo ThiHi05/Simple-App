539e22a8e2f112b18f8a4ced1638db460b074cf5
ThiHi05 <23130098@st.hcmuaf.edu.vn>
2026-06-07 20:56:41 +0700

    Create GameConfig.java
    - Xây dựng cấu trúc dữ liệu lớp GameConfig theo đúng Sơ đồ Class (Class Diagram) của cả nhóm đã thống nhất.
    - Khai báo và đóng gói (Encapsulation) các thuộc tính lưu trữ thông số cấu hình bao gồm: gameSpeed (tốc độ), snakeSkin (màu sắc/giao diện rắn), và difficultyLevel (mức độ khó).
    - Cung cấp các hàm Getter/Setter để các package khác (như startgame, eatfood) có thể truy xuất và áp dụng cấu hình khi trò chơi bắt đầu vận hành.

---
+++ src/configuregame/GameConfig.java
@@ -0,0 +1,22 @@
+package configuregame;
+
+import java.awt.*;
+
+public class GameConfig {
+
+    public int delay;
+
+    public boolean soundEnabled;
+
+    public boolean musicEnabled;
+
+    public boolean multiplayer;
+
+    public String backgroundMusic;
+
+    public Color player1HeadColor;
+    public Color player1BodyColor;
+
+    public Color player2HeadColor;
+    public Color player2BodyColor;
+}
